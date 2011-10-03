deb61c0d69737c3285bdf118b00f3d525acc7717
Hylke Visser <hylke@hylkev.com>
2011-10-03 16:39:09 +0200

    Begonnen met SPARQL queries

--- Sesame/src/nl/bluecode/Sesame.java
+++ Sesame/src/nl/bluecode/Sesame.java
@@ -34,17 +34,18 @@ public class Sesame {
 
 			
 			// Add files to the repository
-			con.add(new File("data/iswc-2006-complete.rdf"), "http://data/iswc2006/", RDFFormat.RDFXML);
-
-			// Retrieve namespaces used in the repository
-			System.out.println("Retrieving namespaces...");
-
+			con.add(new File("data/iswc-2006-complete.rdf"), "http://data/iswc-2006/", RDFFormat.RDFXML);
+			con.add(new File("data/iswc-2008-complete.rdf"), "http://data/iswc-2008/", RDFFormat.RDFXML);
+			con.add(new File("data/iswc-2009-complete.rdf"), "http://data/iswc-2009/", RDFFormat.RDFXML);
+			con.add(new File("data/iswc-2010-complete.rdf"), "http://data/iswc-2010/", RDFFormat.RDFXML);
 			
-
-			System.out.println();
+			con.add(new File("data/www-2007-complete.rdf"), "http://data/www-2007/", RDFFormat.RDFXML);
+			con.add(new File("data/www-2008-complete.rdf"), "http://data/www-2008/", RDFFormat.RDFXML);
+			con.add(new File("data/www-2009-complete.rdf"), "http://data/www-2009/", RDFFormat.RDFXML);
+			con.add(new File("data/www-2010-complete.rdf"), "http://data/www-2010/", RDFFormat.RDFXML);
 			
 			// The query
-			String query = "SELECT ?subject ?object WHERE { ?subject rdf:type ?object }";
+			String query = "SELECT ?class WHERE { ?subject rdf:type ?class }";
 			
 			gui.setOutput(doQuery(query), query);
 			
@@ -65,6 +66,7 @@ public class Sesame {
 			while (namespacesinRepository.hasNext()) {
 				Namespace namespace = namespacesinRepository.next();
 				namespaces += "PREFIX " + namespace.getPrefix() + ": <" + namespace.getName() + ">\n";
+				System.out.println("Namespace " + namespace.getPrefix() + ": <" + namespace.getName() + ">");
 			}
 			
 			tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL, namespaces + query);
